bccdb5a72ea7e2c014e95b3a52b1354e93b6d393
André Lucas <andrelvalentim5@gmail.com>
2024-10-06 21:43:16 -0300

    [Swagger] - Adjusting API documentation

--- src/routes/students-routes/student-routes.ts
+++ src/routes/students-routes/student-routes.ts
@@ -5,47 +5,6 @@ import { findAllPosts } from 'src/controllers/teachers/posts/find-all-posts'
 
 const studentRoutes = Router()
 
-/**
- * @swagger
- * /student/posts:
- *  get:
- *      summary: Returns all posts
- *      responses:
- *          200:
- *           description: A list of posts
- *           content:
- *              application/json:
- *                  schema:
- *                      type: array
- *                      items:
- *                          type: object
- *                          properties:
- *                              id:
- *                                  type: integer
- *                             title:
- *                                 type: string
- *                            content:
- *                               type: string
- *  post:
- *    summary: Create a new post
- *    requestBody:
- *     required: true
- *    content:
- *    application/json:
- *     schema:
- *     type: object
- *    properties:
- *    title:
- *     type: string
- *    content:
- *     type: string
- *   responses:
- *      201:
- *          description: Post created
- *     400:
- *        description: Bad request
- * **/
-
 studentRoutes.get('/posts', findAllPosts)
 studentRoutes.get('/posts/:id_post', FindPostById)
 studentRoutes.get('/search', KeywordSearchPost)
