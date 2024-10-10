cb26aa5f4682e93d36bb3ac9fce742f57c626867
André Lucas <andrelvalentim5@gmail.com>
2024-10-10 20:53:33 -0300

    [Env] - Adding Environment variable

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node:20.14.0
 
+RUN apt-get update && apt-get install -y nano
+
 WORKDIR /app
 
 COPY ./package.json ./
--- src/env/index.ts
+++ src/env/index.ts
@@ -2,23 +2,43 @@ import 'dotenv/config'
 
 import { z } from 'zod'
 
-const envSchema = z.object({
-  PORT: z.coerce.number().default(3000),
-  DATABASE_USER: z.string(),
-  DATABASE_NAME: z.string(),
-  DATABASE_PASSWORD: z.string(),
-  DATABASE_HOST: z.string(),
-  DATABASE_PORT: z.coerce.number(),
-})
+let env
+if (process.env.NODE_ENV === 'development') {
+  const envSchema = z.object({
+    PORT: z.coerce.number().default(3000),
+    DATABASE_USER: z.string(),
+    DATABASE_NAME: z.string(),
+    DATABASE_PASSWORD: z.string(),
+    DATABASE_HOST: z.string(),
+    DATABASE_PORT: z.coerce.number(),
+  })
 
-const _env = envSchema.safeParse(process.env)
+  const _env = envSchema.safeParse(process.env)
 
-if (!_env.success) {
-  console.error(
-    `There's something wrong with the environment variables`,
-    _env.error.format(),
-  )
-  throw new Error(`There's something wrong with the environment variables`)
+  if (!_env.success) {
+    console.error(
+      `There's something wrong with the environment variables`,
+      _env.error.format(),
+    )
+    throw new Error(`There's something wrong with the environment variables`)
+  }
+  env = _env.data
+} else if (process.env.NODE_ENV === 'production') {
+  const envSchema = z.object({
+    PORT: z.coerce.number().default(3000),
+    DATABASE_URL: z.string(),
+  })
+
+  const _env = envSchema.safeParse(process.env)
+
+  if (!_env.success) {
+    console.error(
+      `There's something wrong with the environment variables`,
+      _env.error.format(),
+    )
+    throw new Error(`There's something wrong with the environment variables`)
+  }
+  env = _env.data
 }
 
-export const env = _env.data
+export default env
