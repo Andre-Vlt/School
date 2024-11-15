00e57526c8e86c97d133d8986e69e22fcbb8a9d1
André Lucas <andrelvalentim5@gmail.com>
2024-11-15 17:27:51 -0300

    Fixing getPersonById endpoint

--- src/controllers/administration/person/get-person.ts
+++ src/controllers/administration/person/get-person.ts
@@ -1,10 +1,16 @@
 import express from 'express'
 import { makeGetPersonUseCase } from 'src/use-cases/factory/make-get-person-use-case'
+import { z } from 'zod'
 
 export async function getPerson(req: express.Request, res: express.Response) {
+  const registerParamsSchema = z.object({
+    id: z.string(),
+  })
+  const { id } = registerParamsSchema.parse(req.params)
+
   const getPersonUseCase = makeGetPersonUseCase()
 
-  const person = await getPersonUseCase.handler()
+  const person = await getPersonUseCase.handler(id)
 
   return res.status(200).send(person)
 }
