814d24b15d8e4857f467911c2b3aba8553db3f4d
André Lucas <andrelvalentim5@gmail.com>
2024-11-15 17:36:42 -0300

    Update function name

--- src/routes/administration-routes/adm-routes.ts
+++ src/routes/administration-routes/adm-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { createPerson } from 'src/controllers/administration/person/create-person'
-import { getPerson } from 'src/controllers/administration/person/get-person'
+import { getPersonById } from 'src/controllers/administration/person/get-person'
 import { createStudent } from 'src/controllers/administration/student/create-student'
 import { createTeacher } from 'src/controllers/administration/teacher/create-teacher'
 import { createUser } from 'src/controllers/administration/user/create-user'
@@ -11,6 +11,6 @@ admRouter.post('/user', createUser)
 admRouter.post('/person', createPerson)
 admRouter.post('/teacher', createTeacher)
 admRouter.post('/student', createStudent)
-admRouter.get('/person', getPerson)
+admRouter.get('/person:id', getPersonById)
 
 export default admRouter
