35bea1a5fb78c44aa51c54bfe04eb93da12b2d7b
André Lucas <andrelvalentim5@gmail.com>
2024-11-16 09:16:03 -0300

    Adding CORS

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express from 'express'
 import routes from 'src/routes/index'
 import swaggerUi from 'swagger-ui-express'
 import swaggerOutput from 'src/swagger-output.json'
+import cors from 'cors'
 
 export const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput))
